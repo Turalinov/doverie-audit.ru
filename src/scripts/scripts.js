2988308c6ea6d860b296609889720e1a73ee721b
Ruslan Turalinov <rusltur_24@mail.ru>
2021-08-27 17:11:38 +0600

    added mobile, tablet verstion services block, programming

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -51,6 +51,30 @@
   })
 
 
+  let servicesBtns = document.querySelectorAll('.services__btn');
+  servicesBtns.forEach(servicesBtn => {
+    servicesBtn.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      let btn = e.target;
+      let parent = btn.closest('.services__item');
+      let contents = parent.querySelectorAll('.services__info');
+
+      if (!parent.classList.contains('services__item--opened')) {
+        btn.innerHTML = 'Скрыть'
+      } else {
+        btn.innerHTML = 'Подробнее'
+      }
+
+      parent.classList.toggle("services__item--opened");
+      contents.forEach(content => {
+        content.classList.toggle("services__info--none");
+      });
+
+
+    })
+  })
+
 
 //   const animItems = document.querySelectorAll('._anim-items');
 
